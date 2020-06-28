223876dadabf7fefc8edbec72f255f2ec5ce968e
jibonilla03 <jimmy.03bonilla@gmail.com>
2020-06-28 09:34:51 -0600

    update

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-west-1"
-  access_key = "AKIA4POVFNHMKJNMVDFK"
-  secret_key = "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"
+  //access_key = "Enter access key here"
+  //secret_key = "Enter secret id here"
 }
